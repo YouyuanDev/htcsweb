9027f0a151bdb8aff4eca387144de5821fadd79d
ketewang <ketewang@youyuantech.com>
2018-02-21 17:46:36 +0800

    增加了odcoatingprocessDAO

--- src/main/java/com/htcsweb/dao/OdCoatingProcessDao.java
+++ src/main/java/com/htcsweb/dao/OdCoatingProcessDao.java
@@ -1,14 +1,18 @@
 package com.htcsweb.dao;
 
 import com.htcsweb.entity.OdCoatingProcess;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 public interface OdCoatingProcessDao {
-    public OdCoatingProcess getOdCoatProcessById(int id);
-    public void addOdCoatProcess(OdCoatingProcess odCoatingProcess);
-    public List<OdCoatingProcess> getOdCoatProcess();
-    public void updateOdCoatProcess(OdCoatingProcess odCoatingProcess);
-    public List<OdCoatingProcess> feny(int pagesize, int rows);
-    public int getcount();
+
+
+    public List<HashMap<String,Object>> getAllByLike(@Param("pipe_no") String pipe_no, @Param("operator_no")String operator_no, @Param("begin_time")Date begin_time, @Param("end_time")Date end_time, @Param("skip")int skip, @Param("take")int take);
+    public int getCount();
+    public int addOdCoatingProcess(OdCoatingProcess odcoatingprocess);
+    public int updateOdCoatingProcess(OdCoatingProcess odcoatingprocess);
+    public int delOdCoatingProcess(String[]arrId);
 }
