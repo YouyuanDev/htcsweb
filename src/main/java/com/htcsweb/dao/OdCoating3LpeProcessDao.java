e5d7231263744e205d8219e3eed4e1d69ab92e2e
ketewang <ketewang@youyuantech.com>
2018-02-27 21:54:26 +0800

    OdCoating3LpeProcess.jsp 增加了分厂信息

--- src/main/java/com/htcsweb/dao/OdCoating3LpeProcessDao.java
+++ src/main/java/com/htcsweb/dao/OdCoating3LpeProcessDao.java
@@ -15,8 +15,8 @@ public interface OdCoating3LpeProcessDao {
     public int delOdCoating3LpeProcess(String[] arrId);
     public List<OdCoating3LpeProcess> getOdCoatingProcess();
     public int updateOdCoating3LpeProcess(OdCoating3LpeProcess odCoating3LpeProcess);
-    public List<HashMap<String,Object>> getAllByLike(@Param("pipe_no") String pipe_no, @Param("operator_no") String operator_no, @Param("begin_time") Date begin_time, @Param("end_time") Date end_time, @Param("skip") int skip, @Param("take") int take);
+    public List<HashMap<String,Object>> getAllByLike(@Param("pipe_no") String pipe_no, @Param("operator_no") String operator_no, @Param("begin_time") Date begin_time, @Param("end_time") Date end_time, @Param("mill_no") String mill_no, @Param("skip") int skip, @Param("take") int take);
     //public int getCount();
-    public int getCountAllByLike(@Param("pipe_no") String pipe_no, @Param("operator_no") String operator_no, @Param("begin_time") Date begin_time, @Param("end_time") Date end_time);
+    public int getCountAllByLike(@Param("pipe_no") String pipe_no, @Param("operator_no") String operator_no, @Param("begin_time") Date begin_time, @Param("end_time") Date end_time,@Param("mill_no") String mill_no);
 
 }
