1c8643d7463212f98966989cf7f390adb1975c36
Lfw11 <lfw@youyuantech.com>
2018-03-06 17:52:36 +0800

    2018-03-06
    二维码记录

---
+++ src/main/java/com/htcsweb/dao/PipeQrCodePrintRecordDao.java
@@ -0,0 +1,15 @@
+package com.htcsweb.dao;
+
+import com.htcsweb.entity.PipeQrCodePrintRecord;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
+import java.util.List;
+
+
+public interface PipeQrCodePrintRecordDao {
+
+    public List<PipeQrCodePrintRecord> getQrCodeInfoByLike(@Param("pipe_no") String pipe_no, @Param("operator_no") String operator_no, @Param("begin_time") Date begin_time, @Param("end_time") Date end_time, @Param("skip") int skip, @Param("take") int take);
+    public int getCountQrCodeInfoByLike(@Param("pipe_no") String pipe_no, @Param("operator_no") String operator_no,@Param("begin_time") Date begin_time, @Param("end_time") Date end_time);
+    public int delQrCode(String[] arrId);
+}
