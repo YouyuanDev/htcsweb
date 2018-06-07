d8ba780ff5881c8e99ce9c076e2731de253f5073
ketewang <ketewang@youyuantech.com>
2018-06-08 01:08:58 +0800

    增加了mill的coordinate

--- src/main/java/com/htcsweb/entity/MillInfo.java
+++ src/main/java/com/htcsweb/entity/MillInfo.java
@@ -5,14 +5,16 @@ public class MillInfo {
     private int id;
     private String mill_no;
     private String mill_name;
+    private String coordinate;
 
     public MillInfo() {
     }
 
-    public MillInfo(int id, String mill_no, String mill_name) {
+    public MillInfo(int id, String mill_no, String mill_name, String coordinate) {
         this.id = id;
         this.mill_no = mill_no;
         this.mill_name = mill_name;
+        this.coordinate = coordinate;
     }
 
     public int getId() {
@@ -37,5 +39,13 @@ public class MillInfo {
 
     public void setMill_name(String mill_name) {
         this.mill_name = mill_name;
+    }
+
+    public String getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(String coordinate) {
+        this.coordinate = coordinate;
     }
 }
