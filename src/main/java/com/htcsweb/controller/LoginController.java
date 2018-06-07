d163634789e9e940d3c02774805e376a25e09ca0
ketewang <ketewang@youyuantech.com>
2018-06-07 09:57:03 +0800

    修改登录代码

--- src/main/java/com/htcsweb/controller/LoginController.java
+++ src/main/java/com/htcsweb/controller/LoginController.java
@@ -127,6 +127,7 @@ public class LoginController {
 
                 //跳转到用户主页
                 json.put("success",true);
+                json.put("msg","登录成功");
                 json.put("roles",role_no_list);
                 //System.out.println("登录验证 success");
 //                String basePath = request.getSession().getServletContext().getRealPath("/");
